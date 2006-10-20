3143fe8b3875a27556b648548551c32a84bbd650
perry <perry@d25448e0-c022-0410-b998-a785f7053de0>
2006-10-20 18:50:39 +0000

    Callbacks comecam a funcionar, desconexao da curses funcional.

--- src/log.h
+++ src/log.h
@@ -3,6 +3,8 @@
 
 #define LOG_H_INCLUDED
 
+#include "trace.h"
+
 #ifdef __cplusplus
  extern "C" {
 #endif
@@ -10,52 +12,6 @@
 /*---[ Macros ]---------------------------------------------------------------*/
 
  #include <stdio.h>
-
- #ifdef DEBUG
-    #define TRACE "debug"
- #endif
-
- #ifdef TRACE
-
-    #define DBGFILE stderr
-
-    #define CHKPoint()        	   fprintf(DBGFILE,"%s(%d):\t%s\t\t(" __DATE__ " " __TIME__")\n",__FILE__,__LINE__,__FUNCTION__);fflush(DBGFILE);
-
-    #define DBGMessage(x)     	   fprintf(DBGFILE,"%s(%d):\t%s\n",__FILE__,__LINE__,x);fflush(DBGFILE);
-    #define DBGTrace(x)       	   fprintf(DBGFILE,"%s(%d):\t%s = %ld\n",__FILE__,__LINE__,#x, (unsigned long) x);fflush(DBGFILE);
-    #define DBGTracex(x)      	   fprintf(DBGFILE,"%s(%d):\t%s = %08lx\n",__FILE__,__LINE__,#x, (unsigned long) x);fflush(DBGFILE);
-    #define DBGPrintf(x, ...) 	   fprintf(DBGFILE,"%s(%d):\t" x "\n",__FILE__,__LINE__, __VA_ARGS__);fflush(DBGFILE);
-
- #else
-
-    #define DBGMessage(x) /* x */
-    #define DBGTrace(x) /* x */
-    #define DBGTracex(x) /* x */
-    #define CHKPoint()  /* */
-    #define DBGPrintf(x, ...) /* */
-
- #endif
-
- #ifdef DEBUG
-
-    #define NDBGExec(x, ...)      fprintf(DBGFILE,"%s(%d):\tExecutar: [",__FILE__,__LINE__);fprintf(DBGFILE,x,__VA_ARGS__);fprintf(DBGFILE,"]\n");fflush(DBGFILE);
-    #define MKDir(...)  		  fprintf(DBGFILE,"%s(%d):\tCriar diretorio: [",__FILE__,__LINE__);fprintf(DBGFILE,__VA_ARGS__);fprintf(DBGFILE,"]\n");fflush(DBGFILE);
-
- #else
-
-	#define NDBGExec(x, ...)      g3270_logExec(MODULE, x, __VA_ARGS__)
-	#define MKDir(...) 			  g3270_logExec(MODULE, ini->Get("install","mkdir","mkdir -p \"%s\""), __VA_ARGS__)
-
- #endif
-
-
- #ifndef PACKET
-    #define PACKET __FILE__
- #endif
-
- #ifndef MODULE
-    #define MODULE PACKET
- #endif
 
  #define WriteLog(...)          g3270_log(MODULE, __VA_ARGS__)
  #define WriteError(e,...)      g3270_logRC(MODULE, e, __VA_ARGS__)
