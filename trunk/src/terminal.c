96b2ee05325123755d07b68cc0ccb571442074b7
perry <perry@d25448e0-c022-0410-b998-a785f7053de0>
2006-11-14 13:39:15 +0000

    Melhoria no tratamento de cores.

--- trunk/src/terminal.c
+++ trunk/src/terminal.c
@@ -33,11 +33,18 @@
   // TODO (perry#2#): Read from file(s).
 
   // /usr/X11R6/lib/X11/rgb.txt
-  static const char *TerminalColors  = "black,blue1,red,pink,green1,turquoise,yellow,white,black,DeepSkyBlue,orange,DeepSkyBlue,PaleGreen,PaleTurquoise,grey,white";
-  static const char *FieldColors     = "green1,red,blue,white";
+//  static const char *TerminalColors  = "black,blue1,red,pink,green1,turquoise,yellow,white,black,DeepSkyBlue,orange,DeepSkyBlue,PaleGreen,PaleTurquoise,grey,white";
+  static const char *TerminalColors  = "black,#00FFFF,red,pink,green1,turquoise,yellow,white,black,DeepSkyBlue,orange,DeepSkyBlue,PaleGreen,PaleTurquoise,grey,white";
+
+//  static const char *FieldColors     = "green,red,blue,white";
+  static const char *FieldColors     = "green,red,#00FFFF,white";
+
   static const char *CursorColors	 = "white,white,DarkSlateGray,DarkSlateGray";
   static const char *SelectionColors = "#000020,yellow";
-  static const char *StatusColors	 = "black,ForestGreen,LimeGreen,LimeGreen,red,white,yellow,green,LimeGreen";
+
+// Barra: 7890F0
+//  static const char *StatusColors	 = "black,ForestGreen,LimeGreen,LimeGreen,red,white,yellow,green,LimeGreen";
+  static const char *StatusColors	 = "black,#7890F0,LimeGreen,LimeGreen,red,white,yellow,green,LimeGreen";
 
   static const char *FontDescr[] =
   {
