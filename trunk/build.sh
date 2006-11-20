432f792f850f4cfbf74725f14ae8bf13f749aab4
perry <perry@d25448e0-c022-0410-b998-a785f7053de0>
2006-11-20 16:55:57 +0000

    Incluido script para criacao automatica dos RPMS e posterior envio para o servidor.

---
+++ trunk/build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+OLDDIR=$PWD
+PACKAGE=`basename $OLDDIR`
+
+RPMDIR=`rpm --eval="%{u2p:%{_rpmdir}}"`
+RPMARCH=`rpm --eval="%{u2p:%{_target_cpu}}"`
+VENDOR=`rpm --eval="%{u2p:%{_vendor}}"`
+
+
+mv *~ /tmp
+
+cd ..
+tar -zcvf `rpm --eval="%{u2p:%{_sourcedir}}"`/$PACKAGE.tar.gz $PACKAGE
+
+cd $OLDDIR
+
+rm -f $RPMDIR/$RPMARCH/$PACKAGE*.rpm
+rm -f $RPMDIR/noarch/$PACKAGE*.rpm
+
+rpmbuild -ba $PACKAGE.spec
+
+echo "Enviando arquivo source para o servidor..."
+scp `rpm --eval="%{u2p:%{_srcrpmdir}}"`/$PACKAGE*.src.rpm $USER@os2team.df.intrabb.bb.com.br:/home/html/src/$VENDOR
+
+echo "Enviando arquivo binario o servidor..."
+scp $RPMDIR/$RPMARCH/$PACKAGE*.rpm $RPMDIR/noarch/$PACKAGE*.rpm $USER@os2team.df.intrabb.bb.com.br:/home/html/$VENDOR
+
+
+
