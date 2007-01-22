1fc17fb0ed61aa8d556fb5f1c15a2585bce2d634
perry <perry@d25448e0-c022-0410-b998-a785f7053de0>
2007-01-22 17:59:58 +0000

    Ticket #4: Passa a criar os items de menu 1 por 1 para que, posteriormente, possa ser anexada mais uma string que sera passada para a "action".

--- src/menu.c
+++ src/menu.c
@@ -51,32 +51,13 @@
     int  				qtd		= 0;
     unsigned char		*ptr;
     unsigned char		*ln;
-    int					current = -1;
     int					f;
 
-    GtkItemFactoryEntry	*itens;
+    GtkItemFactoryEntry	item;
 
-    if(!arq)
-    {
-    	Error("Can't load \"%s\"",filename);
-    	return -1;
-    }
+    memset(&item,0,sizeof(item));
 
     while(fgets((char *) buffer,1023,arq))
-    {
-    	if(*buffer == '/')
-    	   qtd++;
-    }
-
-    DBGPrintf("%d menu options",qtd);
-
-    itens = g_malloc(qtd * sizeof(GtkItemFactoryEntry));
-    if(!itens)
-       return -1;
-
-    rewind(arq);
-
-    while(fgets((char *) buffer,1023,arq) && current < qtd)
     {
    	    for(ln=buffer;*ln && *ln <= ' ';ln++);
 
@@ -85,33 +66,46 @@
 
     	if(*buffer == '/')
     	{
-    		current++;
-    		memset(itens+current,0,sizeof(GtkItemFactoryEntry));
+    		if(item.path)
+    		{
+               gtk_item_factory_create_item(	factory,
+												(GtkItemFactoryEntry *) &item,
+												0,
+												1 );
+               qtd++;
+               g_free(item.path);
+    		}
 
-    		(itens+current)->path = g_strdup((char *) buffer);
+            memset(&item,0,sizeof(item));
+    		item.path = g_strdup((char *) buffer);
 
     	}
-    	else if(current >= 0 && *ln && *ln != '#')
+    	else if(item.path && *ln && *ln != '#')
     	{
 		   ptr = (unsigned char *) strchr((char *) ln,'=');
 		   if(ptr)
 		   {
 		      *ptr++ = 0;
-//		      DBGPrintf("%s=\"%s\"",ln,ptr);
-
 		      for(f=0;f< (sizeof(cmd)/sizeof(struct _cmd));f++)
 		      {
 			     if(!strcmp(cmd[f].key,(char *) ln))
-			        cmd[f].exec(itens+current,(char *) ptr);
+			        cmd[f].exec(&item,(char *) ptr);
 		      }
 		   }
     	}
     }
 
-    fclose(arq);
+	if(item.path)
+	{
+       gtk_item_factory_create_item(	factory,
+										(GtkItemFactoryEntry *) &item,
+										0,
+										1 );
+	   qtd++;
+       g_free(item.path);
+	}
 
-	if(current > 0)
-	   gtk_item_factory_create_items(factory, qtd, itens, NULL);
+    fclose(arq);
 
     return qtd;
  }
