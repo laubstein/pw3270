fcac169a1bca2869d0260505bdc8ce6c9e46a8ad
perry <perry@d25448e0-c022-0410-b998-a785f7053de0>
2007-06-21 20:16:32 +0000

    Otimizando o controle de cores.

--- src/colors.c
+++ src/colors.c
@@ -100,114 +100,110 @@
 	 
  }
 
-#ifdef __GTK_COLOR_BUTTON_H__ 
+ #ifdef DEBUG
  
- static void ColorChanged(GtkColorButton *widget, GdkColor *clr)
+ static void InsertInTree(GtkTreeStore *store, GtkTreeIter *parent, const char *str)
  {
-	CHKPoint();
-    gtk_color_button_get_color(widget,clr);
-    gdk_colormap_alloc_color(gtk_widget_get_default_colormap(),clr,TRUE,TRUE);
-    RedrawTerminalContents();
- }
-  
- static void AddColors(int pos, GtkTable *table, const char *string, GdkColor *list, GdkColor *bkp, int count)
- {
-    GtkTable  *colors; 
-	GtkWidget *button;
-	int      f;
-	int      row		= 0;
-	int      col		= 0;
-	gboolean sensitive	= !access(SYSCONFIG,W_OK);
-	 
-    // FIXME: http://developer.gnome.org/doc/API/2.0/gtk/GtkTable.html#gtk-table-attach-defaults	 
-    gtk_table_attach_defaults(table,gtk_label_new( string ), 0,1,pos,pos+1);
-	 
-    colors = GTK_TABLE(gtk_table_new( (count/8),9,1));	 
-	 
-	for(f=0;f<count;f++)
-    {
-		bkp[f] = list[f];
-		button = gtk_color_button_new_with_color(list+f);
-        g_signal_connect(G_OBJECT(button),"color-set",G_CALLBACK(ColorChanged),list+f);
-		
-		gtk_widget_set_sensitive(button,sensitive);
-        gtk_table_attach_defaults(colors,button,col,col+1,row,row+1);
-		if(col++ > 6)
-		{
-			row++;
-			col = 0;
-		}
-    }		
-	 
-    gtk_table_attach_defaults(table,GTK_WIDGET(colors),1,2,pos,pos+1);
-    gtk_table_set_row_spacing(table,pos,4);
-
- }
- 
- static void RestoreColors(GdkColor *clr, GdkColor *bkp, int count)
- {
-	int f;
-	for(f=0;f<count;f++)
-	{
-		clr[f] = bkp[f];
-        gdk_colormap_alloc_color(gtk_widget_get_default_colormap(),clr+f,TRUE,TRUE);
-	}
+	 GtkTreeIter iter;										   
+	 memset(&iter,0,sizeof(iter));
+	 gtk_tree_store_append(store,&iter,parent);
+	 gtk_tree_store_set(store, &iter, 0, str, -1);
  }
  
  void action_set_colors(GtkWidget *w, gpointer data)
  {
-     GdkColor  terminal_backup[TERMINAL_COLORS];
-	 GdkColor  field_backup[FIELD_COLORS];
-     GdkColor  cursor_backup[CURSOR_COLORS];
-     GdkColor  selection_backup[SELECTION_COLORS];
-     GdkColor  status_backup[STATUS_COLORS];
-	 
+	 char *FieldColors[] = {	_( "Normal, Unprotected" ),
+								_( "Intensified, Unprotected" ),
+								_( "Normal, Protected" ),
+								_( "Intensified, Protected" )
+						    };
+							
+	 char *GColors[]	  = {	_( "Blue" ),
+								_( "Red"  ),
+								_( "Pink" ),
+								_( "Green" ),
+							    _( "Turquoise" ),
+							    _( "Yellow" ),
+							    _( "White" ),
+							    _( "Black" ),
+								_( "Dark Blue" ),
+								_( "Orange" ),
+								_( "Purple" ),
+							    _( "Dark Green" ),
+							    _( "Dark Turquoise" ),
+								_( "Mustard" ),
+								_( "Gray" ),
+							    _( "Brown" ) 
+							 };
+		 
 	 GtkWidget *widget;
-	 GtkTable  *table; 
-
+	 GtkWidget *hbox;
+	 GtkWidget *color;
+	 GtkWidget *view;
+	 GtkCellRenderer *renderer;
+	 GtkTreeStore  *store;
+	 GtkTreeIter iter;
+	 int f;
+	 
      widget = gtk_dialog_new_with_buttons (	_( "Configuração de cores" ),
                                             GTK_WINDOW(top_window),
                                             GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT,
                                             GTK_STOCK_OK,     GTK_RESPONSE_ACCEPT,
                                             GTK_STOCK_CANCEL, GTK_RESPONSE_REJECT,
                                             NULL);
-								
-
-     table = GTK_TABLE(gtk_table_new(1,5,0));
-
-	 gtk_table_set_col_spacing(table,0,10);
 	 
-     AddColors(0, table, _( "Terminal" ),  		terminal_cmap,	terminal_backup,	TERMINAL_COLORS );
-	 AddColors(1, table, _( "Fields"),			field_cmap,		field_backup,		FIELD_COLORS);
-     AddColors(2, table, _( "Cursor"),			cursor_cmap,	cursor_backup,		CURSOR_COLORS);
-     AddColors(3, table, _( "Selection Box" ),	selection_cmap,	selection_backup,	SELECTION_COLORS);
-     AddColors(4, table, _( "Status Bar" ),		status_cmap,	status_backup,		STATUS_COLORS);
+	 // Color selection dialog
+	 color = gtk_color_selection_new();
+	 gtk_color_selection_set_has_opacity_control(GTK_COLOR_SELECTION(color),FALSE);
+	 
+	 // Colors tree
+	 view = gtk_tree_view_new();
+	 renderer = gtk_cell_renderer_text_new();
+	 gtk_tree_view_insert_column_with_attributes(GTK_TREE_VIEW(view),-1,"Name",renderer,"text", 0,NULL);
+			
+	 store = gtk_tree_store_new(1, G_TYPE_STRING);
+	 memset(&iter,0,sizeof(iter));
+	 
+	 // Add colors
+	 gtk_tree_store_append(store,&iter,NULL);
+	 gtk_tree_store_set(store, &iter, 0, _( "Base colors" ), -1);
+	 for(f=0;f<(sizeof(FieldColors)/sizeof(char *));f++)
+		 InsertInTree(store,&iter,FieldColors[f]);
+	 
+	 gtk_tree_store_append(store,&iter,NULL);
+	 gtk_tree_store_set(store, &iter, 0, _( "Graphics colors" ), -1);
+	 for(f=0;f<(sizeof(GColors)/sizeof(char *));f++)
+		 InsertInTree(store,&iter,GColors[f]);
 
-	 gtk_container_add(GTK_CONTAINER(GTK_DIALOG(widget)->vbox),GTK_WIDGET(table));
+
+	 gtk_tree_view_set_model(GTK_TREE_VIEW(view), GTK_TREE_MODEL(store));
+
+     g_object_unref(store);
+	 // Boxes
+	 hbox  = gtk_hbox_new(FALSE,2);
+	 gtk_box_pack_start(GTK_BOX(hbox),view,TRUE,TRUE,2);
+	 gtk_box_pack_end(GTK_BOX(hbox),color,TRUE,TRUE,2);
+
+	 gtk_container_add(GTK_CONTAINER(GTK_DIALOG(widget)->vbox),hbox);
 	 gtk_widget_show_all(GTK_WIDGET(GTK_DIALOG(widget)->vbox));
-   	 switch(gtk_dialog_run(GTK_DIALOG(widget)))
+	 
+	 switch(gtk_dialog_run(GTK_DIALOG(widget)))	 
 	 {
 	 case GTK_RESPONSE_ACCEPT:
-		 DBGMessage("Cores confirmadas, atualizar arquivo de configuracao");
-	     break;
-	 
-	 case GTK_RESPONSE_REJECT:
-		 DBGMessage("Alteracao cancelada, restaurar todas as cores");
-     	 RestoreColors(terminal_cmap,	terminal_backup,	TERMINAL_COLORS);
-	 	 RestoreColors(field_cmap,		field_backup,		FIELD_COLORS);
-     	 RestoreColors(cursor_cmap,		cursor_backup,		CURSOR_COLORS);
-     	 RestoreColors(selection_cmap,	selection_backup,	SELECTION_COLORS);
-     	 RestoreColors(status_cmap,		status_backup,		STATUS_COLORS);
 		 break;
 	 
+	 case GTK_RESPONSE_REJECT:
+		 break;
+		 
 	 default:
-	 	 Log("Invalid response from color selection dialog");
+		 Log("Unexpected response from color selection dialog");
 	 }
 	 
      gtk_widget_destroy (widget);
 	 
+	 
      RedrawTerminalContents();
 	 
  }
-
- #endif // __GTK_COLOR_BUTTON_H__
+ 
+#endif
