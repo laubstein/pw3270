c64eaaafe6ef11957f3b502dacfae65fde15bfc3
perry <perry@d25448e0-c022-0410-b998-a785f7053de0>
2008-03-10 16:22:48 +0000

    Empacotamento 3.3.4-21.suse3.3.4-22.suse

---
+++ tags/3.3.4-21.suse3.3.4-22.suse/src/extension.h
@@ -0,0 +1,152 @@
+
+
+#ifndef G3270_EXTENSION_H_INCLUDED
+
+ #include <gtk/gtk.h>
+ #include <sys/time.h>
+
+ #ifdef G3270_CORE
+	 #include <lib/localdefs.h>
+	 #include <lib/lib3270.h>
+ #else
+	 #include <g3270/localdefs.h>
+	 #include <g3270/lib3270.h>
+ #endif
+
+ #define G3270_EXTENSION_H_INCLUDED
+
+ enum status_codes
+ {
+        STATUS_DISCONNECTED,           /*  0 X Not Connected */
+        STATUS_RESOLVING,              /*  1 X Resolving */
+        STATUS_CONNECTING,             /*  2 X Connecting */
+        STATUS_NONSPECIFIC,            /*  3 X */
+        STATUS_INHIBIT,                /*  4 X Inhibit */
+        STATUS_BLANK,                  /*  5 (blank) */
+        STATUS_TWAIT,                  /*  6 X Wait */
+        STATUS_SYSWAIT,                /*  7 X SYSTEM */
+        STATUS_PROTECTED,              /*  8 X Protected */
+        STATUS_NUMERIC,                /*  9 X Numeric */
+        STATUS_OVERFLOW,               /* 10 X Overflow */
+        STATUS_DBCS,                   /* 11 X DBCS */
+        STATUS_SCROLLED,               /* 12 X Scrolled */
+        STATUS_MINUS,                  /* 13 X -f */
+        STATUS_AWAITING_FIRST,	       /* 14 X Wait */
+	    STATUS_CONNECTED,			   /* 15 Connected */
+
+		STATUS_RECONNECTING
+ };
+
+ enum cursor_types
+ {
+ 	CURSOR_TYPE_OVER,
+ 	CURSOR_TYPE_INSERT,
+
+ 	CURSOR_TYPE_CROSSHAIR // Must be the last one!
+ };
+
+ #define CURSOR_TYPE_NONE	-1
+
+ enum _STATUS_COLORS
+ {
+    STATUS_COLOR_BACKGROUND,
+    STATUS_COLOR_SEPARATOR,
+	STATUS_COLOR_CURSOR_POSITION,
+	STATUS_COLOR_LUNAME,
+	STATUS_COLOR_ERROR,
+	STATUS_COLOR_TIME,
+	STATUS_COLOR_WARNING,
+	STATUS_COLOR_NORMAL,
+	STATUS_COLOR_TOOGLE,
+	STATUS_COLOR_SSL,
+	STATUS_COLOR_CONNECTED,
+	STATUS_COLOR_KEYBOARD,
+	STATUS_COLOR_CONNECT_ICON,
+
+	STATUS_COLORS	// Must be the last one
+ };
+
+ #define STATUS_COLOR_CNCT 		STATUS_COLOR_CONNECT_ICON
+ #define STATUS_COLOR_TYPEAHEAD STATUS_COLOR_TOOGLE
+
+ #ifndef GDK_NUMLOCK_MASK
+    #define GDK_NUMLOCK_MASK GDK_MOD2_MASK
+ #endif
+
+ #ifndef GDK_ALT_MASK
+     #define GDK_ALT_MASK GDK_MOD1_MASK
+ #endif
+
+ #define LockThreads()		gdk_lock()
+ #define UnlockThreads()	CHKPoint(); gdk_unlock()
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+/*---[ Macros ]---------------------------------------------------------------*/
+
+ #define Log(...)               g3270_log(MODULE, __VA_ARGS__)
+ #define WriteLog(...)          g3270_log(MODULE, __VA_ARGS__)
+ #define WriteError(e,...)      g3270_logRC(MODULE, e, __VA_ARGS__)
+
+ #define Error(...)             g3270_logRC(MODULE, -1, __VA_ARGS__)
+ #define ErrorPopup(...)        g3270_popup(MODULE, -1, __VA_ARGS__)
+
+ #define Exec(...)              g3270_logExec(MODULE, __VA_ARGS__)
+ #define NOT_IMPLEMENTED( ... ) g3270_log(MODULE, "*** NOT IMPLEMENTED CALL ***: " __FILE__ " " __VA_ARGS__)
+
+ #if defined( DEBUG )
+
+    #define DBGFILE stderr
+
+    #define CHKPoint()        	   fprintf(DBGFILE,"%s(%d):\t%s\t\t(" __DATE__ " " __TIME__")\n",__FILE__,__LINE__,__FUNCTION__);fflush(DBGFILE);
+
+    #define DBGMessage(x)     	   fprintf(DBGFILE,"%s(%d):\t%s\n",__FILE__,__LINE__,x);fflush(DBGFILE);
+    #define DBGTrace(x)       	   fprintf(DBGFILE,"%s(%d):\t%s = %ld\n",__FILE__,__LINE__,#x, (unsigned long) x);fflush(DBGFILE);
+    #define DBGTracex(x)      	   fprintf(DBGFILE,"%s(%d):\t%s = %08lx\n",__FILE__,__LINE__,#x, (unsigned long) x);fflush(DBGFILE);
+    #define DBGPrintf(x, ...) 	   fprintf(DBGFILE,"%s(%d):\t" x "\n",__FILE__,__LINE__, __VA_ARGS__);fflush(DBGFILE);
+
+ #else
+
+    #define DBGMessage(x) /* x */
+    #define DBGTrace(x) /* x */
+    #define DBGTracex(x) /* x */
+    #define CHKPoint()  /* */
+    #define DBGPrintf(x, ...) /* */
+
+ #endif
+
+ #ifndef BUILD
+    #define BUILD 20070425
+ #endif
+
+
+/*---[ Prototipes ]-----------------------------------------------------------*/
+
+ int	g3270_logRC(const char *, int, const char *, ...);
+ int	g3270_popup(const char *, int, const char *, ...);
+
+ int	g3270_log(const char *, const char *, ...);
+ int	g3270_logExec(const char *, const char *, ...);
+ int	g3270_logName(const char *);
+
+ int	g3270_lock(void);
+ int	g3270_unlock(void);
+
+ void	gdk_lock(void);
+ void	gdk_unlock(void);
+
+/*---[ Extension entry points ]-----------------------------------------------*/
+
+ int  		g3270OpenExtension(GtkWidget *TopWindow);
+ int  		g3270CloseExtension(GtkWidget *TopWindow);
+ void 		g3270LUChanged(GtkWidget *widget, const char *lu);
+ void 		g3270ServerChanged(GtkWidget *widget, const char *server);
+
+#ifdef __cplusplus
+ }
+#endif
+
+
+#endif
---
+++ tags/3.3.4-21.suse3.3.4-22.suse/src/screen_callbacks.c
@@ -0,0 +1,393 @@
+
+#include <gdk/gdk.h>
+#include <gdk/gdkkeysyms.h>
+
+#include "g3270.h"
+#include "lib/kybdc.h"
+
+/*---[ Defines ]--------------------------------------------------------------*/
+
+#define TCNT    7
+#define LUCNT   8
+#define CM     (60 * 10)
+
+/*---[ Prototipes ]-----------------------------------------------------------*/
+
+static void screen_init(void);
+static void screen_disp(Boolean erasing, const struct ea *display);
+static void screen_suspend(void);
+
+static void screen_resume(void);
+static void screen_type(const char *model_name, int maxROWS, int maxCOLS);
+
+static void cursor_move(int baddr);
+static void cursor_mode(int mode);
+
+static void toggle_monocase(struct toggle *t, enum toggle_type tt);
+static void screen_changed(int first, int last);
+
+static void status_ctlr_done(void);
+static void status_insert_mode(Boolean on);
+static void status_minus(void);
+static void status_reset(void);
+static void status_reverse_mode(Boolean on);
+static void status_syswait(void);
+static void status_twait(void);
+static void status_typeahead(Boolean on);
+static void status_compose(Boolean on, unsigned char c, enum keytype keytype);
+static void status_lu(const char *lu);
+static void status_oerr(int error_type);
+static void ring_bell(void);
+static void screen_flip(void);
+static void screen_width(int width);
+static void error_popup(const char *msg);
+static void Redraw_action(Widget w, XEvent *event, String *params, Cardinal *num_params);
+static void status_timing(struct timeval *t0, struct timeval *t1);
+static	const char *lookup_cname(int ccode);
+
+/*---[ Globals ]--------------------------------------------------------------*/
+
+ char oia_cursor[8]					= "";
+ char oia_LUName[12]				= "          ";
+ char oia_Timet[TCNT+1]				= "";
+ char oia_Timer[OIA_TIMER_COUNT+1]	= "";
+ char oia_undera					= 1;
+ char oia_Typeahead[2] 				= " ";
+
+/*---[ 3270 Screen callback table ]-------------------------------------------*/
+
+const SCREEN_CALLBACK g3270_screen_callbacks =
+{
+    sizeof(SCREEN_CALLBACK),
+	SCREEN_MAGIC,
+
+    screen_init,
+    screen_disp,
+    screen_suspend,
+
+	screen_resume,
+	screen_type,
+    screen_changed,
+
+	cursor_move,
+    cursor_mode,
+
+	toggle_monocase,
+
+	status_ctlr_done,
+	status_insert_mode,
+	status_minus,
+	status_oerr,
+	status_reset,
+	status_reverse_mode,
+	status_syswait,
+	status_twait,
+	status_typeahead,
+	status_compose,
+	status_lu,
+
+	ring_bell,
+	screen_flip,
+	screen_width,
+
+	lookup_cname,
+
+	error_popup,
+
+	Redraw_action,
+
+	status_timing,
+	status_untiming
+
+};
+
+/*---[ Keyboard tables ]------------------------------------------------------*/
+
+ /* I "think" this isn't necessary anymore */
+
+ static const KEYTABLE keys[] =
+ {
+	{ "DOWN",		GDK_Down 		},
+	{ "UP",			GDK_Up 			},
+	{ "LEFT",		GDK_Left		},
+	{ "RIGHT",		GDK_Right 		},
+	{ "BACKSPACE",	GDK_BackSpace	},
+	{ CN, 0 }
+
+ };
+
+ const KEYBOARD_INFO g3270_keyboard_info =
+ {
+	sizeof(KEYBOARD_INFO),
+	KEYBOARD_MAGIC,
+
+	ring_bell,
+	gsource_addfile,
+	gsource_removefile,
+
+	keys
+
+ };
+
+/*---[ Implement screen callbacks ]-------------------------------------------*/
+
+static void screen_init(void)
+{
+	CHKPoint();
+}
+
+static void screen_disp(Boolean erasing, const struct ea *display)
+{
+	RedrawTerminalContents();
+}
+
+static void screen_suspend(void)
+{
+	CHKPoint();
+}
+
+static void screen_resume(void)
+{
+	RedrawTerminalContents();
+}
+
+static void screen_type(const char *model_name, int maxROWS, int maxCOLS)
+{
+	DBGPrintf("Screen type: %s (%dx%d)",model_name,maxROWS,maxCOLS);
+}
+
+static void cursor_move(int baddr)
+{
+	int		row;
+	int		col;
+ 	int		cols;
+
+ 	Get3270DeviceBuffer(0, &cols);
+
+ 	row = baddr/cols;
+ 	col = baddr - (row*cols);
+
+    SetCursorPosition(row,col);
+    snprintf(oia_cursor,7,"%02d/%03d",row+1,col+1);
+
+}
+
+static void toggle_monocase(struct toggle *t, enum toggle_type tt)
+{
+	CHKPoint();
+}
+
+/* Done with controller confirmation */
+static void status_ctlr_done(void)
+{
+	oia_undera = 1;
+	RedrawStatusLine();
+}
+
+static void status_insert_mode(Boolean on)
+{
+	if(on)
+	   SetCursorType(CURSOR_TYPE_INSERT);
+	else
+	   SetCursorType(CURSOR_TYPE_OVER);
+}
+
+/* Lock the keyboard (X -f) */
+static void status_minus(void)
+{
+	SetOIAStatus(STATUS_MINUS);
+}
+
+/* Unlock the keyboard */
+static void status_reset(void)
+{
+   if (GetKeyboardStatus() & KL_ENTER_INHIBIT)
+      SetOIAStatus(STATUS_INHIBIT);
+   else if (GetKeyboardStatus() & KL_DEFERRED_UNLOCK)
+      SetOIAStatus(STATUS_NONSPECIFIC);
+   else
+      SetOIAStatus(STATUS_BLANK);
+}
+
+/* Toggle reverse mode */
+static void status_reverse_mode(Boolean on)
+{
+	NotImplemented();
+}
+
+/* Lock the keyboard (X SYSTEM) */
+static void status_syswait(void)
+{
+	SetOIAStatus(STATUS_SYSWAIT);
+}
+
+/* Lock the keyboard (twait) */
+static void status_twait(void)
+{
+	oia_undera = 0;
+    SetOIAStatus(STATUS_TWAIT);
+}
+
+/* Toggle typeahead */
+static void status_typeahead(Boolean on)
+{
+	DBGPrintf("Typeahead: %s",on ? "Yes" : "No");
+	*oia_Typeahead = on ? 'T' : ' ';
+    RedrawStatusLine();
+}
+
+/* Set compose character */
+static void status_compose(Boolean on, unsigned char c, enum keytype keytype)
+{
+	DBGPrintf("Compose: %s Char: %02x \"%c\"",on ? "Yes" : "No", c, c);
+	NotImplemented();
+
+    RedrawStatusLine();
+}
+
+/* Set LU name */
+static void status_lu(const char *lu)
+{
+    if(!lu)
+       lu = "";
+
+    g3270_log("lib3270", "Using LU \"%s\"",lu);
+    snprintf(oia_LUName,11,"%-10s",lu);
+    UpdateWindowTitle();
+    RedrawStatusLine();
+
+#ifdef EXTENSIONS
+	SetExtensionsChar("g3270LUChanged",lu);
+#endif
+
+}
+
+static void ring_bell(void)
+{
+#ifdef DEBUG
+    DBGMessage("Beep!");
+	if(terminal && terminal->window)
+	   gdk_beep();
+#endif
+}
+
+static void screen_flip(void)
+{
+	CHKPoint();
+}
+
+static void screen_width(int width)
+{
+    // FIXME (perry#1#): Recalculate font size!
+   DBGTrace(width);
+   RedrawTerminalContents();
+}
+
+static void error_popup(const char *msg)
+{
+	Log(msg);
+}
+
+static void Redraw_action(Widget w, XEvent *event, String *params, Cardinal *num_params)
+{
+	RedrawTerminalContents();
+}
+
+static void screen_changed(int first, int last)
+{
+   WaitForScreen = FALSE;
+//   action_remove_selection(0,0);
+   RedrawTerminalContents();
+}
+
+static void status_oerr(int error_type)
+{
+   switch(error_type)
+   {
+   case KL_OERR_PROTECTED:
+      SetOIAStatus(STATUS_PROTECTED);
+      break;
+
+   case KL_OERR_NUMERIC:
+      SetOIAStatus(STATUS_NUMERIC);
+      break;
+
+   case KL_OERR_OVERFLOW:
+      SetOIAStatus(STATUS_OVERFLOW);
+      break;
+
+   case KL_OERR_DBCS:
+      SetOIAStatus(STATUS_DBCS);
+      break;
+
+   }
+
+}
+
+#ifdef DEBUG
+   static int dbgTimerEnabled = 0;
+#endif
+
+static void status_timing(struct timeval *t0, struct timeval *t1)
+{
+   unsigned long cs;       // centiseconds
+
+#ifdef DEBUG
+   if(!dbgTimerEnabled)
+   {
+      dbgTimerEnabled = 1;
+      DBGMessage("Timer ON!");
+   }
+#endif
+
+   if (t1->tv_sec - t0->tv_sec > (99*60))
+   {
+      strncpy(oia_Timer,":??.?",OIA_TIMER_COUNT);
+   }
+   else
+   {
+      cs = (t1->tv_sec - t0->tv_sec) * 10 +
+           (t1->tv_usec - t0->tv_usec + 50000) / 100000;
+
+      if (cs < CM)
+         snprintf(oia_Timer, OIA_TIMER_COUNT, ":%02ld.%ld", cs / 10, cs % 10);
+      else
+         snprintf(oia_Timer, OIA_TIMER_COUNT, "%02ld:%02ld", cs / CM, (cs % CM) / 10);
+   }
+
+   RedrawStatusLine();
+
+}
+
+void status_untiming(void)
+{
+#ifdef DEBUG
+   if(dbgTimerEnabled)
+   {
+      dbgTimerEnabled = 0;
+      DBGMessage("Timer Off!");
+   }
+#endif
+
+   *oia_Timer = 0;
+   RedrawStatusLine();
+}
+
+static void cursor_mode(int mode)
+{
+	DBGPrintf("********** CURSOR: %d",mode);
+	SetTerminalCursorByID(mode);
+}
+
+const char *lookup_cname(int ccode)
+{
+	int i;
+
+	for(i = 0; i < (sizeof(keys) / sizeof(KEYTABLE));i++)
+	{
+		if(ccode == keys[i].code)
+			return keys[i].name;
+	}
+
+	return CN;
+}
