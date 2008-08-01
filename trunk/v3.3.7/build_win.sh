bbf968cfd936f680836fa5465f9767296b902e1a
perry <perry@d25448e0-c022-0410-b998-a785f7053de0>
2008-08-01 18:28:39 +0000

    Workin in progress.

--- trunk/v3.3.7/build_win.sh
+++ trunk/v3.3.7/build_win.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 IMAGE=image.w32
-LOCALE=$IMAGE/locale
-PACKET=$PWD/g3270_`date +%G%m%d%H%M`.zip
+LOCALE=locale
 
 rm -fr bin/Release
 make Release
@@ -17,41 +16,6 @@ if [ "$?" != "0" ]; then
 	exit -1
 fi
 
-mv bin/Release/g3270.exe $IMAGE
-if [ "$?" != "0" ]; then
-	exit -1
-fi
-
-mv bin/Release/*.dll $IMAGE
-if [ "$?" != "0" ]; then
-	exit -1
-fi
-
-cp image/*.jpg $IMAGE/g3270.jpg
-if [ "$?" != "0" ]; then
-	exit -1
-fi
-
-cp src/g3270/ui.xml $IMAGE
-if [ "$?" != "0" ]; then
-	exit -1
-fi
-
-cp src/g3270/fonts.conf $IMAGE
-if [ "$?" != "0" ]; then
-	exit -1
-fi
-
-cp g3270.conf $IMAGE
-if [ "$?" != "0" ]; then
-	exit -1
-fi
-
-#cp actions.win $IMAGE/actions.conf
-#if [ "$?" != "0" ]; then
-#	exit -1
-#fi
-
 mkdir -p $LOCALE/pt_BR/LC_MESSAGES
 if [ "$?" != "0" ]; then
 	exit -1
@@ -62,11 +26,3 @@ if [ "$?" != "0" ]; then
 	exit -1
 fi
 
-mv g3270_*.zip /tmp
-cd $IMAGE
-zip -9 -r $PACKET *
-if [ "$?" != "0" ]; then
-	exit -1
-fi
-
-echo $PACKET gerado!
