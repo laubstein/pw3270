4f403cf03a3cafb2ed2af0f604fcff6f6904884f
perry <perry@d25448e0-c022-0410-b998-a785f7053de0>
2008-08-19 19:14:57 +0000

    Otimizando o "arremedo de configure" para win32.

--- v3.3.7/build_win.sh
+++ v3.3.7/build_win.sh
@@ -9,22 +9,24 @@ GTK_MODULES="glib-2.0 gtk+-2.0 gthread-2.0"
 SSL_MODULES="libcrypto libssl openssl"
 
 TEMPFILE=`mktemp`
-echo "s#@PACKAGE_NAME@#$PACKAGE_NAME#g;" > $TEMPFILE
-echo "s#@PACKAGE@#$PACKAGE_NAME#g;" >> $TEMPFILE
-echo "s#@CC@#mingw32-gcc#g;" >> $TEMPFILE
-echo "s#@PACKAGE_VERSION@#$PACKAGE_VERSION#g;" >> $TEMPFILE
-echo "s#@GTK_CFLAGS@#`pkg-config --cflags $GTK_MODULES`#g;" >> $TEMPFILE
-echo "s#@GTK_LIBS@#`pkg-config --libs $GTK_MODULES`#g;" >> $TEMPFILE
-echo "s#@SSL_CFLAGS@#`pkg-config --cflags $SSL_MODULES`#g;" >> $TEMPFILE
-echo "s#@SSL_LIBS@#`pkg-config --libs $SSL_MODULES`#g;" >> $TEMPFILE
-echo "s#@NATIVECC@#gcc#g;" >> $TEMPFILE
-echo "s#@WINDRES@#mingw32-windres#g;" >> $TEMPFILE
-echo "s#@OS_LIBS@#-lws2_32#g;" >> $TEMPFILE
-echo "s#@XCPPFLAGS@#-D_WIN32 -DWC3270 -D_WIN32_WINNT=0x0500#g;" >> $TEMPFILE
-echo "s#@DLLEXT@#dll#g;" >> $TEMPFILE
-echo "s#@OBJEXT@#o#g;" >> $TEMPFILE
-echo "s#@BINEXT@#.exe#g;" >> $TEMPFILE
-echo "s#@EXTRASRC@#resources.rc#g;" >> $TEMPFILE
+echo "s&@PACKAGE_NAME@&$PACKAGE_NAME&g;" > $TEMPFILE
+echo "s&@PACKAGE@&$PACKAGE_NAME&g;" >> $TEMPFILE
+echo "s&@CC@&mingw32-gcc&g;" >> $TEMPFILE
+echo "s&@PACKAGE_VERSION@&$PACKAGE_VERSION&g;" >> $TEMPFILE
+echo "s&@GTK_CFLAGS@&`pkg-config --cflags $GTK_MODULES`&g;" >> $TEMPFILE
+echo "s&@GTK_LIBS@&`pkg-config --libs $GTK_MODULES`&g;" >> $TEMPFILE
+echo "s&@SSL_CFLAGS@&`pkg-config --cflags $SSL_MODULES`&g;" >> $TEMPFILE
+echo "s&@SSL_LIBS@&`pkg-config --libs $SSL_MODULES`&g;" >> $TEMPFILE
+echo "s&@NATIVECC@&gcc&g;" >> $TEMPFILE
+echo "s&@WINDRES@&mingw32-windres&g;" >> $TEMPFILE
+echo "s&@OS_LIBS@&-lws2_32&g;" >> $TEMPFILE
+echo "s&@XCPPFLAGS@&-D_WIN32 -DWC3270 -D_WIN32_WINNT=0x0500&g;" >> $TEMPFILE
+echo "s&@DLLEXT@&dll&g;" >> $TEMPFILE
+echo "s&@OBJEXT@&o&g;" >> $TEMPFILE
+echo "s&@BINEXT@&.exe&g;" >> $TEMPFILE
+echo "s&@EXTRASRC@&resources.rc&g;" >> $TEMPFILE
+echo "s&#undef PACKAGE_NAME&#define PACKAGE_NAME \"$PACKAGE_NAME\"&g;" >> $TEMPFILE
+echo "s&#undef PACKAGE_VERSION&#define PACKAGE_VERSION \"$PACKAGE_VERSION\"&g;" >> $TEMPFILE
 
 mv ~/Desktop/g3270_installer.exe ~/tmp > /dev/null 2>&1
 
@@ -34,6 +36,11 @@ if [ "$?" != "0" ]; then
 fi
 
 make clean
+
+sed --file=$TEMPFILE src/g3270/config.h.in > src/g3270/config.h
+if [ "$?" != "0" ]; then
+	exit -1
+fi
 
 sed --file=$TEMPFILE g3270.nsi.in > g3270.nsi
 if [ "$?" != "0" ]; then
