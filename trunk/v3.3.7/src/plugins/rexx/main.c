2c94c4f99b82c4db7bd8a5231f8c75118da21641
perry <perry@d25448e0-c022-0410-b998-a785f7053de0>
2008-09-15 17:06:00 +0000

    Trabalhando no plugin rexx.

--- trunk/v3.3.7/src/plugins/rexx/main.c
+++ trunk/v3.3.7/src/plugins/rexx/main.c
@@ -35,6 +35,7 @@
  /* include the "C" stuff */
  #include <string.h>
  #include <sys/time.h>                   /* System time-related data types */
+ #include <stdlib.h>
 
  /* include the lib3270 stuff */
  #define G3270_MODULE_NAME "rexx"
@@ -309,6 +310,56 @@
 	return RetValue(Retstr,0);
  }
 
+ ULONG APIENTRY rx3270Sleep(PSZ Name, LONG Argc, RXSTRING Argv[],PSZ Queuename, PRXSTRING Retstr)
+ {
+	clock_t end = clock();
+
+	switch(Argc)
+	{
+	case 0:
+		end += CLOCKS_PER_SEC;
+		break;
+
+	case 1:
+		end += (atoi(Argv[0].strptr) * CLOCKS_PER_SEC);
+		break;
+
+	default:
+		return RXFUNC_BADCALL;
+	}
+
+	while(clock() < end)
+		gtk_main_iteration();
+
+	return RetValue(Retstr,0);
+ }
+
+ ULONG APIENTRY rx3270FindFieldAttribute(PSZ Name, LONG Argc, RXSTRING Argv[],PSZ Queuename, PRXSTRING Retstr)
+ {
+	if(Argc != 1)
+		return RXFUNC_BADCALL;
+
+	return RetValue(Retstr,find_field_attribute(atoi(Argv[0].strptr)));
+ }
+
+ ULONG APIENTRY rx3270FindFieldLength(PSZ Name, LONG Argc, RXSTRING Argv[],PSZ Queuename, PRXSTRING Retstr)
+ {
+	if(Argc != 1)
+		return RXFUNC_BADCALL;
+
+	return RetValue(Retstr,find_field_length(atoi(Argv[0].strptr)));
+ }
+
+ ULONG APIENTRY rx3270MoveCursor(PSZ Name, LONG Argc, RXSTRING Argv[],PSZ Queuename, PRXSTRING Retstr)
+ {
+	if(Argc != 1)
+		return RXFUNC_BADCALL;
+
+	cursor_move(atoi(Argv[0].strptr));
+
+	return RetValue(Retstr,0);
+ }
+
  #define EXPORT_REXX_FUNCTION(x) { #x, (PFN) x }
 
  static const struct _entrypoint
@@ -317,16 +368,20 @@
  	PFN				call;
  } entrypoint[] =
  {
-	EXPORT_REXX_FUNCTION( rx3270Version		),
-	EXPORT_REXX_FUNCTION( rx3270Actions		),
-	EXPORT_REXX_FUNCTION( rx3270ToggleON	),
-	EXPORT_REXX_FUNCTION( rx3270ToggleOFF	),
-	EXPORT_REXX_FUNCTION( rx3270Toggle		),
-	EXPORT_REXX_FUNCTION( rx3270Toggled		),
-	EXPORT_REXX_FUNCTION( rx3270Log			),
-	EXPORT_REXX_FUNCTION( rx3270QueryCState	),
-	EXPORT_REXX_FUNCTION( rx3270Popup 		),
-	EXPORT_REXX_FUNCTION( rx3270UpdateScreen ),
+	EXPORT_REXX_FUNCTION( rx3270Version				),
+	EXPORT_REXX_FUNCTION( rx3270Actions				),
+	EXPORT_REXX_FUNCTION( rx3270ToggleON			),
+	EXPORT_REXX_FUNCTION( rx3270ToggleOFF			),
+	EXPORT_REXX_FUNCTION( rx3270Toggle				),
+	EXPORT_REXX_FUNCTION( rx3270Toggled				),
+	EXPORT_REXX_FUNCTION( rx3270Log					),
+	EXPORT_REXX_FUNCTION( rx3270QueryCState			),
+	EXPORT_REXX_FUNCTION( rx3270Popup 				),
+	EXPORT_REXX_FUNCTION( rx3270UpdateScreen		),
+	EXPORT_REXX_FUNCTION( rx3270Sleep				),
+	EXPORT_REXX_FUNCTION( rx3270FindFieldAttribute	),
+	EXPORT_REXX_FUNCTION( rx3270FindFieldLength		),
+	EXPORT_REXX_FUNCTION( rx3270MoveCursor			)
  };
 
 /*---[ Implement ]--------------------------------------------------------------------------------*/
