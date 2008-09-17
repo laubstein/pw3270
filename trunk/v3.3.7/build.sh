9740d7ea9e9d91f3288bfcc99eeb001aea647580
perry <perry@d25448e0-c022-0410-b998-a785f7053de0>
2008-09-17 13:52:17 +0000

    V 3.3.7 passa a ser a versao default.

--- trunk/v3.3.7/build.sh
+++ trunk/v3.3.7/build.sh
@@ -72,11 +72,13 @@ if [ "$?" != "0" ]; then
 	exit -1
 fi
 
-echo "Enviando arquivo source para o servidor..."
-scp `rpm --eval="%{u2p:%{_srcrpmdir}}"`/g3270*.src.rpm $USER@os2team.df.intrabb.bb.com.br:public_html/g3270
-if [ "$?" != "0" ]; then
-	echo "Erro ao copiar o pacote fonte"
-	exit -1
+if [ "$1" != "--debug" ]; then
+	echo "Enviando arquivo source para o servidor..."
+	scp `rpm --eval="%{u2p:%{_srcrpmdir}}"`/g3270*.src.rpm $USER@suportelinux.df.bb.com.br:src/suse/g3270-latest.src.rpm
+	if [ "$?" != "0" ]; then
+		echo "Erro ao copiar o pacote fonte"
+		exit -1
+	fi
 fi
 
 echo "Enviando arquivo binario para o servidor..."
@@ -86,28 +88,11 @@ if [ "$?" != "0" ]; then
 	exit -1
 fi
 
-#if [ "$1" != "--debug" ]; then
-#   echo Gerando copia do repositorio para a tag $PACKET_VERSION
-
-#   if [ "$VENDOR" = "suse" ]; then
-#   	svn rm --force -m "Empacotamento $PACKET_VERSION" http://$USER@suportelinux.df.bb.com.br/svn/$PACKAGE/tags/$PACKET_VERSION
-#   	svn cp -m "Empacotamento $PACKET_VERSION" http://$USER@suportelinux.df.bb.com.br/svn/$PACKAGE/trunk http://$USER@suportelinux.df.bb.com.br/svn/$PACKAGE/tags/$PACKET_VERSION
-#   fi
-
-#  echo "Enviando arquivo source para o servidor..."
-#   scp `rpm --eval="%{u2p:%{_srcrpmdir}}"`/$PACKAGE*.src.rpm $USER@suportelinux.df.bb.com.br:/dados/src/$VENDOR/$PACKAGE-latest.src.rpm
-#   if [ "$?" != "0" ]; then
-#      echo "Erro ao copiar o pacote fonte"
-#      exit -1
-#   fi
-
-#   echo "Enviando arquivo binario o servidor..."
-#   scp $RPMDIR/$RPMARCH/$PACKAGE*.rpm $USER@os2team.df.intrabb.bb.com.br:/home/matriz/pacotes/$VENDOR/bb/$RPMARCH
-#   if [ "$?" != "0" ]; then
-#      echo "Erro ao copiar o pacote binario"
-#   fi
-
-#fi
+scp $RPMDIR/$RPMARCH/g3270*.rpm $USER@os2team.df.intrabb.bb.com.br:/home/matriz/pacotes/$VENDOR/bb/$RPMARCH
+if [ "$?" != "0" ]; then
+	echo "Erro ao copiar o pacote binario"
+	exit -1
+fi
 
 ./configure --enable-plugins --prefix=/home/perry/bin/g3270
 
