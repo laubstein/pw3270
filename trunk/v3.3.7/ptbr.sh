7cb93199b9743c9da52be34440db21aba6cc2718
perry <perry@d25448e0-c022-0410-b998-a785f7053de0>
2008-11-07 10:30:15 +0000

    Corrigindo tradução da função "Smart Paste".

--- trunk/v3.3.7/ptbr.sh
+++ trunk/v3.3.7/ptbr.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 make g3270.pot
 cp g3270.pot po/pt_BR.po
-gtranslator --auto-translate=po/pt_BR.po
-gtranslator po/pt_BR.po
+gtranslator --auto-translate=po/pt_BR.po > /dev/null 2>&1
+gtranslator po/pt_BR.po > /dev/null 2>&1
 rm -f ~/.gtranslator/umtf/personal-learn-buffer.xml
 printf "po/pt_BR.po\ny" | gtranslator-build-learn-buffer
 mkdir -p locale/pt_BR/LC_MESSAGES
