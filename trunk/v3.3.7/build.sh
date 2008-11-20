c72d90a97f7e3c5a33b6e99c7120e302bef3b924
perry <perry@d25448e0-c022-0410-b998-a785f7053de0>
2008-11-20 16:22:36 +0000

    Ticket #65: Implementando funcao "paste" com o botao central do mouse.

--- trunk/v3.3.7/build.sh
+++ trunk/v3.3.7/build.sh
@@ -104,6 +104,7 @@ BuildRPM() {
 		TARGET_FOLDER="$USER@storage:/dados/suse/$(rpm --eval="%{suse_version}" | cut -b1-2)/bb/$RPMARCH"
 	fi
 
+	echo "Copiando pacotes para o servidor..."
 	scp $RPMDIR/$RPMARCH/g3270*.rpm $TARGET_FOLDER
 	if [ "$?" != "0" ]; then
 		echo "Erro ao copiar o pacote binario"
