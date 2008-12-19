ddf049ecd618a1ee2f7320abca09ddaecf1c22b0
perry <perry@d25448e0-c022-0410-b998-a785f7053de0>
2008-12-19 11:42:03 +0000

    Removendo chamadas que nao sao usadas no g3270.

--- v3.3.7/src/lib/actions.c
+++ v3.3.7/src/lib/actions.c
@@ -1,27 +1,27 @@
-/* 
+/*
  * "Software G3270, desenvolvido com base nos códigos fontes do WC3270  e  X3270
  * (Paul Mattes Paul.Mattes@usa.net), de emulação de terminal 3270 para acesso a
  * aplicativos mainframe.
- * 
+ *
  * Copyright (C) <2008> <Banco do Brasil S.A.>
- * 
+ *
  * Este programa é software livre. Você pode redistribuí-lo e/ou modificá-lo sob
  * os termos da GPL v.2 - Licença Pública Geral  GNU,  conforme  publicado  pela
  * Free Software Foundation.
- * 
+ *
  * Este programa é distribuído na expectativa de  ser  útil,  mas  SEM  QUALQUER
  * GARANTIA; sem mesmo a garantia implícita de COMERCIALIZAÇÃO ou  de  ADEQUAÇÃO
  * A QUALQUER PROPÓSITO EM PARTICULAR. Consulte a Licença Pública Geral GNU para
  * obter mais detalhes.
- * 
+ *
  * Você deve ter recebido uma cópia da Licença Pública Geral GNU junto com este
  * programa;  se  não, escreva para a Free Software Foundation, Inc., 59 Temple
  * Place, Suite 330, Boston, MA, 02111-1307, USA
- * 
+ *
  * Este programa está nomeado como actions.c e possui 877 linhas de código.
- * 
- * Contatos: 
- * 
+ *
+ * Contatos:
+ *
  * perry.werneck@gmail.com	(Alexandre Perry de Souza Werneck)
  * erico.mendonca@gmail.com	(Erico Mascarenhas Mendonça)
  * licinio@bb.com.br		(Licínio Luis Branco)
@@ -199,9 +199,9 @@ XtActionsRec all_actions[] = {
 	{ "FieldEnd",		FieldEnd_action },
 	{ "FieldMark",		FieldMark_action },
 	{ "HexString",		HexString_action},
-#if defined(C3270) || defined(WC3270) /*[*/
-	{ "Help",		Help_action},
-#endif/*]*/
+// #if defined(C3270) || defined(WC3270) /*[*/
+//	{ "Help",		Help_action},
+// #endif/*]*/
 #if defined(X3270_SCRIPT) /*[*/
 	{ "Plugin",		Plugin_action},
 #endif/*]*/
