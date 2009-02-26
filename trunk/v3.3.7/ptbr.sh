39ead1a822ec3434b15f5792cef44b5d7b9382dc
perry <perry@d25448e0-c022-0410-b998-a785f7053de0>
2009-02-26 14:18:04 +0000

    Atualizando tradução.

--- trunk/v3.3.7/ptbr.sh
+++ trunk/v3.3.7/ptbr.sh
@@ -4,7 +4,7 @@ cp g3270.pot po/pt_BR.po
 gtranslator --auto-translate=po/pt_BR.po > /dev/null 2>&1
 gtranslator po/pt_BR.po > /dev/null 2>&1
 rm -f ~/.gtranslator/umtf/personal-learn-buffer.xml
-printf "po/pt_BR.po\ny" | gtranslator-build-learn-buffer
+printf "po/pt_BR.po\ny" | /usr/share/gtranslator/scripts/build-gtranslator-learn-buffer.sh
 mkdir -p locale/pt_BR/LC_MESSAGES
 if [ "$?" != "0" ]; then
 	exit -1
