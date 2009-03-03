8087351c340afdf172008f97d837be8c9f937afe
perry <perry@d25448e0-c022-0410-b998-a785f7053de0>
2009-03-03 17:32:58 +0000

    Incluindo parametro de linha de comando para mudar o titulo da janela.

--- v3.3.7/src/g3270/g3270.c
+++ v3.3.7/src/g3270/g3270.c
@@ -457,6 +457,7 @@ static void load_options(GOptionContext *context)
 		{ "host",				'h', 0, G_OPTION_ARG_STRING,	&cl_hostname,			N_( "Host identifier" ),									NULL },
 		{ "startup-script", 	's', 0, G_OPTION_ARG_FILENAME, 	&startup_script,		N_( "Run script on startup (if available)" ),				NULL },
 		{ "program-data",	 	'd', 0, G_OPTION_ARG_STRING, 	&program_data,			N_( "Path to search for data and configuration files" ),	NULL },
+		{ "window-title",	 	'd', 0, G_OPTION_ARG_STRING, 	&window_title,			N_( "Main window title" ),									PACKAGE_NAME },
 
 #ifdef HAVE_PLUGINS
 		{ "plugins",	 		'p', 0, G_OPTION_ARG_STRING, 	&plugin_list,			N_( "Full path of plugins to load" ),						NULL },
