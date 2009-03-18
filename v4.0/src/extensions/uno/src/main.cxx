43f10aa5ad56d6f4ca0e98772cb335f6316843f7
perry <perry@d25448e0-c022-0410-b998-a785f7053de0>
2009-03-18 16:47:42 +0000

    Código linux funcionando com a classe "helper".

--- v4.0/src/extensions/uno/src/main.cxx
+++ v4.0/src/extensions/uno/src/main.cxx
@@ -145,20 +145,6 @@ extern "C" void * SAL_CALL component_getFactory(const sal_Char * pImplName, void
 
 	if(pServiceManager && rtl_str_compare( pImplName, IMPLNAME ) == 0)
 	{
-/*
-		Reference< XSingleComponentFactory > xFactory( ::cppu::createSingleComponentFactory(
-		reinterpret_cast< XMultiServiceFactory * >( pServiceManager ),
-		OUString::createFromAscii( IMPLNAME ),
-		CreateInstance_g3270, getSupportedServiceNames_g3270() ) );
-
-
-		if (xFactory.is())
-		{
-			xFactory->acquire();
-			pRet = xFactory.get();
-		}
-*/
-
 		Reference< XSingleComponentFactory > xFactory( ::cppu::createSingleComponentFactory(
 					CreateInstance_g3270, OUString::createFromAscii( IMPLNAME ), getSupportedServiceNames_g3270() ));
 
@@ -198,113 +184,6 @@ Sequence< OUString > g3270::uno_impl::getSupportedServiceNames() throw (RuntimeE
 {
 	return getSupportedServiceNames_g3270();
 }
-
-/*---[ XInterface implementation ]-------------------------------------------------------------------------*/
-
-/*
-void SAL_CALL g3270::uno_impl::acquire() throw ()
-{
-	Trace("%s",__FUNCTION__);
-//	++m_nRefCount;
-}
-
-void SAL_CALL g3270::uno_impl::release() throw ()
-{
-	Trace("%s",__FUNCTION__);
-//	if (! --m_nRefCount)
-//		delete this;
-}
-
-*/
-
-/*
-Any g3270::uno_impl::queryInterface( Type const & type ) throw (RuntimeException)
-{
-	if (type.equals( ::cppu::UnoType< Reference< XInterface > >::get()))
-	{
-		// return XInterface interface (resolve ambiguity caused by multiple inheritance from
-		// XInterface subclasses by casting to lang::XTypeProvider)
-		Trace("%s(type).equals %s",__FUNCTION__,"XInterface");
-		Reference< XInterface > x( static_cast< lang::XTypeProvider * >( this ) );
-		return makeAny( x );
-
-	}
-	if (type.equals( ::cppu::UnoType< Reference< lang::XTypeProvider > >::get()))
-	{
-		// return XInterface interface
-		Trace("%s(type).equals %s",__FUNCTION__,"lang::XTypeProvider");
-		Reference< lang::XTypeProvider > x( static_cast< lang::XTypeProvider * >( this ) );
-		return makeAny( x );
-
-	}
-	if (type.equals( ::cppu::UnoType< Reference< lang::XServiceInfo > >::get()))
-	{
-		// return XServiceInfo interface
-		Trace("%s(type).equals %s",__FUNCTION__,"lang::XServiceInfo");
-		Reference< lang::XServiceInfo > x( static_cast< lang::XServiceInfo * >( this ) );
-		return makeAny( x );
-
-	}
-	if (type.equals( ::cppu::UnoType< Reference< I3270 > >::get()))
-	{
-		// return sample interface
-		Trace("%s(type).equals %s",__FUNCTION__,"I3270");
-		Reference< I3270 > x( static_cast< I3270 * >( this ) );
-		return makeAny( x );
-
-	}
-
-	// querying for unsupported type
-	Trace("%s(type).equals %s",__FUNCTION__,"Unsupported");
-
-	return Any();
-
-}
-*/
-
-/*---[ Implement XTypeProvider ]---------------------------------------------------------------------------*/
-
-/*
-Sequence<sal_Int8> SAL_CALL I3270Impl::getImplementationId(void) throw (RuntimeException)
-{
-	static Sequence< sal_Int8 > * s_pId = 0;
-
-	Trace("%s",__FUNCTION__);
-
-	if(! s_pId)
-	{
-		// create unique id
-		Sequence< sal_Int8 > id( 16 );
-
-		::rtl_createUuid( (sal_uInt8 *)id.getArray(), 0, sal_True );
-
-		// guard initialization with some mutex
-		::osl::MutexGuard guard( ::osl::Mutex::getGlobalMutex() );
-		if (! s_pId)
-		{
-			static Sequence< sal_Int8 > s_id( id );
-			s_pId = &s_id;
-		}
-	}
-	return *s_pId;
-
-}
-
-Sequence< Type > SAL_CALL I3270Impl::getTypes(  ) throw (RuntimeException)
-{
-	// http://wiki.services.openoffice.org/wiki/Documentation/DevGuide/WritingUNO/C%2B%2B/Implementing_without_Helpers
-	Sequence< Type > seq( 3 );
-
-	Trace("%s begin",__FUNCTION__);
-
-	seq[ 0 ] = ::cppu::UnoType< Reference< lang::XTypeProvider > >::get();
-	seq[ 1 ] = ::cppu::UnoType< Reference< lang::XServiceInfo > >::get();
-	seq[ 2 ] = ::cppu::UnoType< Reference< I3270 > >::get();
-
-	Trace("%s ends",__FUNCTION__);
-	return seq;
-}
-*/
 
 /*---[ Implement I3270 ]-----------------------------------------------------------------------------------*/
 
