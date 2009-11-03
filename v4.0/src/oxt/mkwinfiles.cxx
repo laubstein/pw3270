cbfb50afc0685927e4a2c991ff0060c6dae329ff
perry.werneck <perry.werneck@gmail.com>
2009-11-03 09:06:55 +0000

    Ainda tentando gerar o oxt para windows.

--- v4.0/src/oxt/mkwinfiles.cxx
+++ v4.0/src/oxt/mkwinfiles.cxx
@@ -48,6 +48,43 @@ int main(int numpar, char *param[])
 	fprintf(out,"<manifest:file-entry manifest:media-type=\"application/vnd.sun.star.uno-component;type=native;platform=Windows\" manifest:full-path=\"windows/pw3270.uno.dll\"/>\n");
 	fprintf(out,"<manifest:file-entry manifest:media-type=\"application/vnd.sun.star.uno-typelibrary;type=RDB\" manifest:full-path=\"pw3270.uno.rdb\"/>\n");
 	fprintf(out,"</manifest:manifest>\n");
+
+	fclose(out);
+
+	FILE *out = fopen(param[2],"w");
+
+	if(!out)
+		return -1;
+
+	fprintf(out,"<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n");
+	fprintf(out,"<description xmlns=\"http://openoffice.org/extensions/description/2006\"\n");
+	fprintf(out,"xmlns:d=\"http://openoffice.org/extensions/description/2006\"\n");
+	fprintf(out,"xmlns:xlink=\"http://www.w3.org/1999/xlink\">\n");
+
+	fprintf(out,"  <version value="4.0" />\n");
+
+	fprintf(out,"  <identifier value=\"pw3270\" />\n");
+
+	fprintf(out,"  <platform value=\"windows_x86\" />\n");
+
+	fprintf(out,"  <dependencies>\n");
+	fprintf(out,"    <OpenOffice.org-minimal-version value=\"2.2\" d:name=\"OpenOffice.org 2.2\"/>\n");
+	fprintf(out,"  </dependencies>\n");
+
+	fprintf(out,"  <display-name>\n");
+	fprintf(out,"    <name lang=\"en\">3270 access extension</name>\n");
+	fprintf(out,"  </display-name>\n");
+
+	fprintf(out,"  <icon>\n");
+	fprintf(out,"    <default xlink:href=\"pw3270.png\" />\n");
+	fprintf(out,"  </icon>\n");
+
+	fprintf(out,"  <extension-description>\n");
+	fprintf(out,"    <src xlink:href=\"description.txt\" lang=\"en\" />\n");
+	fprintf(out,"  </extension-description>\n");
+
+	fprintf(out,"</description>\n");
+
 	fclose(out);
 
 	return 0;
