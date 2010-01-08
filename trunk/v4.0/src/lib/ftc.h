6bceb89c75e6dc16dcebf4aa756964fa8747a060
perry.werneck@gmail.com <perry.werneck@gmail.com@d25448e0-c022-0410-b998-a785f7053de0>
2010-01-08 17:06:32 +0000

    Melhorando mensagems.

--- trunk/v4.0/src/lib/ftc.h
+++ trunk/v4.0/src/lib/ftc.h
@@ -29,6 +29,7 @@ LIB3270_INTERNAL char *ft_local_filename;
 LIB3270_INTERNAL Boolean ft_last_cr;
 LIB3270_INTERNAL Boolean remap_flag;
 
+LIB3270_INTERNAL void ft_init(void);
 LIB3270_INTERNAL void ft_aborting(void);
 LIB3270_INTERNAL void ft_complete(const char *errmsg);
 LIB3270_INTERNAL void ft_running(Boolean is_cut);
